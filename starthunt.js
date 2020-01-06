1bdd054d933799bcbea8182527866dd72820b770
Kekoa <koaminami@aol.com>
2020-01-06 01:30:18 -0800

    First Run

--- starthunt.js
+++ starthunt.js
@@ -1,10 +1,17 @@
-var jsonData = JSON.parse('/start.json');
-                          
-while (jsonData.starthunt == false) {
-    // ...
-    jsonData = JSON.parse("https://raw.githubusercontent.com/TofuBytesRBX/BeachIV-Scavenger-Hunt/master/start.json");
-    if (jsonData.starthunt == true) {
+var starthunt = false
+
+do {
+    $.ajax({
+  url: 'https://raw.githubusercontent.com/TofuBytesRBX/BeachIV-Scavenger-Hunt/master/start.txt',
+  dataType: 'json',
+  success: function(data){
+    console.log( data );
+      starthunt = data;
+  }
+});
+    if (starthunt == true);
         window.location.replace("https://tofubytesrbx.github.io/BeachIV-Scavenger-Hunt/start.html");
-        break;
-    } 
+    break;
 }
+
+while (starthunt == false);
