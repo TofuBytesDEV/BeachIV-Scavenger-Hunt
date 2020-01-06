21ee11eeebce7076907a424a4740a6ea6503e22d
Kekoa <koaminami@aol.com>
2020-01-06 01:53:09 -0800

    Update starthunt.js

--- starthunt.js
+++ starthunt.js
@@ -1,17 +1,10 @@
-var starthunt = false
+$ npm install p-wait-for
 
-do {
-    $.ajax({
-  url: 'https://raw.githubusercontent.com/TofuBytesRBX/BeachIV-Scavenger-Hunt/master/start.txt',
-  dataType: 'jsonp',
-  success: function(data){
-    console.log( data );
-      starthunt = data;
-  }
-});
-    if (starthunt == true);
-        window.location.replace("https://tofubytesrbx.github.io/BeachIV-Scavenger-Hunt/start.html");
-    break;
-}
+const pWaitFor = require('p-wait-for');
+const pathExists = require('path-exists');
 
-while (starthunt == false);
+(async () => {
+	await pWaitFor(() => pathExists('/go.txt'));
+	console.log('Yay! The file now exists.');
+    window.location.replace("https://tofubytesrbx.github.io/BeachIV-Scavenger-Hunt/start.html");
+})();
