17e7f08446e4e39abeb38597ab0b5574d7d60e95
Kekoa <koaminami@aol.com>
2020-01-06 01:03:21 -0800

    Update starthunt.js

--- starthunt.js
+++ starthunt.js
@@ -2,7 +2,7 @@ var jsonData = JSON.parse('/start.json');
                           
 while (jsonData.starthunt == false) {
     // ...
-    jsonData = JSON.parse("/start.json");
+    jsonData = JSON.parse("https://raw.githubusercontent.com/TofuBytesRBX/BeachIV-Scavenger-Hunt/master/start.json");
     if (jsonData.starthunt == true) {
         window.location.replace("https://tofubytesrbx.github.io/BeachIV-Scavenger-Hunt/start.html");
         break;
